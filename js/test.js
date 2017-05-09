7595ed957ead8f75dbbbe77e256573c440172743
Emmanuel Smith <Emmanuel Smith>
2017-05-09 15:29:26 +0100

    Exercise 3 complete

--- js/test.js
+++ js/test.js
@@ -1,20 +1,15 @@
 (function($) {
-  function loadProducts() {
-    $.getJSON("js/test.json", function(data) {
-      var ProductHTML = "",
-          container = $(".main");
-        $.each(data.heroList.heroItems, function (index, item) {
-          var baseUrl = data.baseSite,
-              imageUrl = baseUrl + item.imageUrl,
-              href = baseUrl + item.cta.link,
-              productTemplate = '<div class="wrapper"><div class="left"><img src="' + imageUrl + '" alt="" /><h1>' + item.headlineText + '</h1><a href="' + href + '">' + item.cta.text + '</a></div></div>';
-          ProductHTML += productTemplate;
-          console.log(imageUrl);
-        });
-        container.empty().append(ProductHTML);
-    }).fail(function() {
-      console.log('This is a failed request!!');
+  function hideSidebar() {
+    $('.sidebar').on('click', function(e){
+      e.preventDefault();
+      if($('.sidebarBlock').hasClass('visible')) {
+        $('.sidebarBlock').removeClass('visible');
+        $('.sidebarBlock').fadeIn(800);
+      }else {
+        $('.sidebarBlock').addClass('visible');
+        $('.sidebarBlock').fadeOut(800);
+      }
     });
   }
-  $(document).ready(loadProducts);
+  $(document).ready(hideSidebar);
 })(jQuery);
